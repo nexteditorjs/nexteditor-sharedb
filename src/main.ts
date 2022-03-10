4dd286b7b4383c00c2d94b2dbe49cac8694487e8
nexteditorjs <nexteditorjs@gmail.com>
2022-03-10 19:21:14 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { assert, createEditor, editorBlocks } from '@nexteditorjs/nexteditor-core';
+import { assert, createEditor } from '@nexteditorjs/nexteditor-core';
 import TableBlock from '@nexteditorjs/nexteditor-table-block';
 import './style.css';
 import ShareDBDoc from './sharedb-doc';
-
-editorBlocks.registerComplexBlockClass(TableBlock);
 
 const app = document.querySelector<HTMLDivElement>('#app');
 assert(app, 'app does not exists');
@@ -14,8 +12,11 @@ ShareDBDoc.load({
   collectionName: 'app-1',
   documentId: 'doc-12',
 }).then((doc) => {
-  console.log(doc);
-  const editor = createEditor(app, doc);
+  const editor = createEditor(app, doc, {
+    components: {
+      blocks: [TableBlock],
+    },
+  });
   (window as any).editor = editor;
 }).catch((err) => {
   console.error(err);
