5aa2afc30b4148a91ecbe2316feca34477b8bcde
nexteditorjs <nexteditorjs@gmail.com>
2022-04-30 14:44:58 +0800

    refactor

--- src/remote-cursor-decorator/remote-cursor-decorator.ts
+++ src/remote-cursor-decorator/remote-cursor-decorator.ts
@@ -1,15 +1,19 @@
-import { assert, DocBlockText, DocInsertion, genId, getLogger, getTextLength, NextEditor, NextEditorDecorator, removeClass } from '@nexteditorjs/nexteditor-core';
+import { assert, BlockPath, DocBlockText, DocInsertion, genId, getLogger, getTextLength, NextEditor, NextEditorDecorator } from '@nexteditorjs/nexteditor-core';
 import ShareDBDoc from '../sharedb-doc';
 
 const logger = getLogger('remote-cursor-decorator');
 
 export class RemoteCursorDecorator implements NextEditorDecorator {
-  decorateText(editor: NextEditor, containerId: string, blockIndex: number, blockText: DocBlockText): { insertions?: Map<number, DocInsertion[]>, text?: DocBlockText } {
+  decorateText(editor: NextEditor, path: BlockPath, blockText: DocBlockText): { insertions?: Map<number, DocInsertion[]>, text?: DocBlockText } {
     //
     const externDoc = editor.doc.externalDoc;
     assert(logger, externDoc instanceof ShareDBDoc, 'invalid doc type');
     //
     const insertions = new Map<number, DocInsertion[]>();
+    //
+    const { containerId, blockIndex } = path[path.length - 1];
+    assert(logger, typeof containerId === 'string', 'invalid path containerId');
+    assert(logger, typeof blockIndex === 'number', 'invalid path blockIndex');
     //
     const client = externDoc.client;
     const blockData = editor.doc.getBlockData(containerId, blockIndex);
